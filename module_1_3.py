7461162879f8027070545da351028ad5d994c634
Sony <super.kuznec-77@yandex.ru>
2024-09-21 20:58:02 +0300

    21/09

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,8 @@
 name='Kuznec'
 print(name, type(name))
-age=46
+age= 46
 print(age, type(age))
-new_age=46+5
-print(new_age, type(new_age))
-is_student=25>5
+age= age+5
+print(age, type(age))
+is_student= 25>5
 print(is_student, type(is_student))
