e565708db786617242ab45390bba6fc9b2acf7c2
Sony <super.kuznec-77@yandex.ru>
2024-09-21 15:59:19 +0300

    21/09

---
+++ main.py
@@ -0,0 +1,9 @@
+first_name='Kurs:Python'
+print(first_name)
+task=12
+print(task)
+second_name='ucheba'
+print(second_name)
+Time_for_the_task=1.5
+print(Time_for_the_task)
+print(Time_for_the_task / task)
