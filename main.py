f33a73313d960cdc5e9d90ac365abea6fd424180
Kuznec77 <super.kuznec-77@yandex.ru>
2024-09-21 21:40:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-first_name='Kurs:Python'
-print(first_name)
+name='Kurs:Python'
+print(name)
 task=12
 print(task)
-second_name='ucheba'
-print(second_name)
-Time_for_the_task=1.5
-print(Time_for_the_task)
-print(Time_for_the_task / task)
+second= 1.5
+print(second)
+print(second / task)
